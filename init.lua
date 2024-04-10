2485ca006cd64698245e334d3d019ce244ec684c
wendtalexander <wendtalexander@protonmail.com>
2024-04-10 10:16:10 +0200

    adding neogen

--- init.lua
+++ init.lua
@@ -906,6 +906,23 @@ require('lazy').setup({
     main = 'ibl',
     opts = {},
   },
+  {
+    'danymat/neogen',
+    config = true,
+    -- Uncomment next line if you want to follow only stable versions
+    -- version = "*"
+    opt = {
+      enabled = true,
+      languages = {
+        python = {
+          template = {
+            annotation_convention = 'numpydoc',
+          },
+        },
+      },
+    },
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
