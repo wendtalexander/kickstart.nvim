815142195cea73ed249482e5f261d732e89c2aa7
wendtalexander <wendtalexander@protonmail.com>
2024-04-15 07:04:24 +0200

    adding pdf reader

--- init.lua
+++ init.lua
@@ -896,6 +896,7 @@ require('lazy').setup({
       'lervag/vimtex',
       init = function()
         vim.g['vimtex_compiler_method'] = 'latexmk'
+        vim.g['vimtex_view_method'] = 'zathura'
       end,
     },
   },
