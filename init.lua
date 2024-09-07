71d0a0725cd02cf8c6985029d1e05fc9ee497f12
wendtalexander <wendtalexander@protonmail.com>
2024-09-07 15:46:10 +0200

    updating neogen

--- init.lua
+++ init.lua
@@ -857,7 +857,7 @@ require('lazy').setup({
       }
       vim.cmd.colorscheme 'rose-pine'
 
-      -- You can configure highlights by doing something like:
+      -- -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
