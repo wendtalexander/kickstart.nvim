74f7906ca751d4fd019e7d1c6d8aca6dad06fd3a
wendtalexander <wendtalexander@protonmail.com>
2024-11-04 07:51:14 +0100

    [ruff] formating

--- init.lua
+++ init.lua
@@ -717,7 +717,13 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        python = function(bufnr)
+          if require('conform').get_formatter_info('ruff_format', bufnr).available then
+            return { 'ruff_format' }
+          else
+            return { 'isort', 'black' }
+          end
+        end,
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
