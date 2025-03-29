138e43b69823f798a4b27f79e6b880a0886bc124
wendtalexander <wendtalexander@protonmail.com>
2025-03-29 18:30:48 +0100

    removing format on save, oil settings

--- init.lua
+++ init.lua
@@ -974,24 +974,25 @@ require('lazy').setup({
   },
   {
     'stevearc/oil.nvim',
+		config = function()
+			require("oil").setup({
+				columns = { "icon" },
+				keymaps = {
+					["<C-h>"] = false,
+					["<C-l>"] = false,
+					["<C-k>"] = false,
+					["<C-j>"] = false,
+					["<M-h>"] = "actions.select_split",
+				},
+				view_options = { show_hidden = true },
+			})
+
+			-- Open parent directory in current window
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		end,
     -- Optional dependencies
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('oil').setup {
-        columns = { 'icon' },
-        keymaps = {
-          ['<C-h>'] = false,
-          ['<C-l>'] = false,
-          ['<C-k>'] = false,
-          ['<C-j>'] = false,
-          ['<M-h>'] = 'actions.select_split',
-        },
-        view_options = { show_hidden = true },
-      }
 
-      -- Open parent directory in current window
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-    end,
   },
   {
     'jiaoshijie/undotree',
